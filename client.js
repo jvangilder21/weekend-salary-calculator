34ccb2e9fb0e2d59cb320b3fd67c6a994de61abc
Jenny Van Gilder <jkosty21@gmail.com>
2023-07-19 13:37:56 -0500

    Was able to get salaries to commit and add to the DOM

--- client.js
+++ client.js
@@ -1,4 +1,8 @@
+
 console.log('Weekend Salary Calculator');
+
+let employees = [];
+const monthlyCostTotal = document.getElementById('totalMonthlyCost');
 
 
 function submitForm(event){
@@ -13,7 +17,7 @@ function submitForm(event){
     console.log('ID number', idNumber);
     let title = document.querySelector('#titleInput').value;
     console.log('title', title);
-    let salary = document.querySelector('#salaryInput').value;
+    let salary = Number(document.querySelector('#salaryInput').value);
     console.log('salary', salary);
 
     console.log('Our values to log all in one log', firstName, lastName, idNumber, title, salary);
@@ -26,45 +30,65 @@ function submitForm(event){
             <td>${lastName}</td>
             <td>${idNumber}</td>
             <td>${title}</td>
-            <td>${salary}</td>
+            <td>$${salary}</td>
             <td><button onClick="removeRow(event)">Delete</button></td>
         </tr>
     </tbody>
     `
+
+    // Create employee object
+    const employee = {
+        firstName: firstName,
+        lastName: lastName,
+        idNumber: idNumber,
+        title: title,
+        salary: salary
+      };
+
+    // Add employee to the list
+    employees.push(employee);
+
     document.getElementById("employeeInfoForm").reset();
+    // Calculate monthly costs and update the DOM
+    calculateMonthlyCosts();
+
+};
+    //This somewhat works for now
 
     // let sum = 0;
+    // let totalMonthlySum = 0;
     // let totalAnnualSalaries = [];
     // totalAnnualSalaries.push(salary);
 
     // for(let i = 0; i < totalAnnualSalaries.length; i++){
     //     sum += totalAnnualSalaries[i];
-    // };
-    // console.log('our sal sum', totalAnnualSalaries);
+    //     console.log("Each Emp's Sal", sum);
+    //     let monthlyCost = (sum / 12).toFixed(2);
+    //     console.log("Each Emp's Sal", monthlyCost);
+    //     //document.querySelector('#totalMonthlyCost').innerHTML = `$${monthlyCost}`;
+    //     totalMonthlySum += monthlyCost;
+    //     document.querySelector('#totalMonthlyCost').innerHTML = `$${totalMonthlySum}`;
 
-    let monthlyCost = salary / 12;
-    console.log(monthlyCost);
-    // let totalMonthlyCost = document.querySelector('#totalMonthlyCost').value;
-    // totalMonthlyCost += monthlyCost;
-    // totalMonthlyCost.innerHTML = `
-    // <h2>$${totalMonthlyCost}</h2>
-    // `
+    // };  
 
-}
+function calculateMonthlyCosts() {
+    let totalMonthlyCosts = 0;
+    employees.forEach(function(employee) {
+        totalMonthlyCosts += employee.salary / 12;
+    });
+    //console.log('tot month cost var', totalMonthlyCosts);
 
-
+    monthlyCostTotal.innerHTML = `Total Monthly Costs: $${totalMonthlyCosts.toFixed(2)}`;
+    //Add red background color if total exceeds $20,000
+    
+};
+        
 function removeRow(event){
     event.target.closest('tr').remove();
 }
 
-/*
 
+// ## Assignment
 
-## Assignment
-
- Using the stored information, 
- calculate monthly costs and append this to the to DOM. 
- If the total monthly cost exceeds $20,000, add a red background 
- color to the total monthly cost.
-
-*/
+//  If the total monthly cost exceeds $20,000, add a red 
+// background color to the total monthly cost.
