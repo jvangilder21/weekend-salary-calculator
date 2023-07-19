089b245c23e20f7c3ec00be47cbd0308fc5e1d18
Jenny Van Gilder <jkosty21@gmail.com>
2023-07-19 13:51:29 -0500

    Added the BG color for salaries over 0000

--- client.js
+++ client.js
@@ -10,17 +10,17 @@ function submitForm(event){
     console.log('submit Form');
 
     let firstName = document.querySelector('#firstNameInput').value;
-    console.log('first name', firstName);
+    //console.log('first name', firstName);
     let lastName = document.querySelector('#lasttNameInput').value;
-    console.log('last name', lastName);
+    //console.log('last name', lastName);
     let idNumber = document.querySelector('#idInput').value;
-    console.log('ID number', idNumber);
+    //console.log('ID number', idNumber);
     let title = document.querySelector('#titleInput').value;
-    console.log('title', title);
+    //console.log('title', title);
     let salary = Number(document.querySelector('#salaryInput').value);
-    console.log('salary', salary);
+    //console.log('salary', salary);
 
-    console.log('Our values to log all in one log', firstName, lastName, idNumber, title, salary);
+    //console.log('Our values to log all in one log', firstName, lastName, idNumber, title, salary);
 
     let employeeTableUpdate = document.querySelector('#employeeTableData');
     employeeTableUpdate.innerHTML += `
@@ -80,15 +80,25 @@ function calculateMonthlyCosts() {
 
     monthlyCostTotal.innerHTML = `Total Monthly Costs: $${totalMonthlyCosts.toFixed(2)}`;
     //Add red background color if total exceeds $20,000
-    
+    if (totalMonthlyCosts > 20000){
+        document.getElementById("totalMonthlyCost").style.backgroundColor = "red";
+    }
 };
         
 function removeRow(event){
     event.target.closest('tr').remove();
 }
 
+/*
+## Stretch Mode
 
-// ## Assignment
+Add styling or extra functionality that fits with the theme 
+of this assignment.
 
-//  If the total monthly cost exceeds $20,000, add a red 
-// background color to the total monthly cost.
+Once the employee is deleted, update the _Total Monthly 
+Cost_ section on the page to reflect the employee's 
+removal. _HINT:_ You will need to figure out which employee 
+was removed, in order to subtract their salary from the total. 
+This is tricky! 
+*/
+
